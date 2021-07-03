4b4b976229bd53ea4777aa05ef2e1b271b762c7c
Shabbir-D <SD53786110@gmail.com>
2021-07-03 09:59:45 +0530

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -22,30 +22,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Set the recipient email address.
     // FIXME: Update this to your desired email address.
-    $recipient = "miskulhusain53@gmail.com";
+    echo $recipient = "miskulhusain53@gmail.com";
 
     // Set the email subject.
-    $subject = "New contact from $name";
+    echo $subject = "New contact from $name";
 
     // Build the email content.
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     // $email_content .= "Subject: $cont_subject\n";
-    $email_content .= "Message:\n$message\n";
+    echo $email_content .= "Message:\n$message\n";
 
     // Build the email headers.
-    $email_headers = "From: $name <$email>";
+    echo $email_headers = "From: $name <$email>";
 
     // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } else {
-        // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
+    // if (mail($recipient, $subject, $email_content, $email_headers)) {
+    // Set a 200 (okay) response code.
+    // http_response_code(200);
+    // echo "Thank You! Your message has been sent.";
+    // } else {
+    // Set a 500 (internal server error) response code.
+    // http_response_code(500);
+    // echo "Oops! Something went wrong and we couldn't send your message.";
+    // }
 } else {
     // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
